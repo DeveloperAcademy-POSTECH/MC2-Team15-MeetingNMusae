58ad77368c8aa916ee885a7ad402865531666e0e
Ji Woong Kang <ipkjw2@gmail.com>
2022-06-11 12:31:18 +0900

    #13 [WIP] feat: add RoleSelectView and selected role View

--- MeetingNMusae/MeetingNMusae/Views/RoleSelectView.swift
+++ MeetingNMusae/MeetingNMusae/Views/RoleSelectView.swift
@@ -7,52 +7,129 @@
 
 import SwiftUI
 
+let imageWidthSize: CGFloat = 128
+let imageHeightSize: CGFloat = 86
+
 struct RoleSelectView: View {
+    @State var columns: [GridItem] = [
+        GridItem(),
+        GridItem()
+    ]
+    
+    @State var roomCode: String
+    
+    @State var roles: [Role] = []
+    
     @State var roleImages: [Image] = [
         Image("진행무새"),
         Image("기록무새"),
         Image("타임무새"),
         Image("주제무새"),
         Image("이해무새"),
-        Image("왜??무새"),
+        Image("왜무새"),
         Image("삐딱무새"),
         Image("좋아무새"),
         Image("발언권무새"),
         Image("금고무새")
     ]
-    @State private var viewModel = RoleListViewModel()
     
+    @ObservedObject var roleViewModel = RoleViewModel()
+    
+    init(roomCode: String) {
+        self.roomCode = roomCode
+    }
     
     var body: some View {
-        List(viewModel.roles) { role in
-            RoleItem(roleImage: roleImages[role.id], roleName: role.roleName)
+        VStack(alignment: .leading) {
+            Text("역할을 골라주세요").font(.headline).padding(.leading)
+            HStack{
+                Image(systemName: "star.circle.fill")
+                Text("필수 역할입니다")
+//                Button(action: {
+//                    for role in roles {
+//                        print("\(role.roleName)", terminator: " ")
+//                    }
+//                }, label: {
+//                    Text("show all roles")
+//                })
+            }
+            .padding(.leading)
+            .font(.subheadline)
+            .foregroundColor(Color.gray)
+            
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(roleViewModel.roles) { role in
+//                        roles.append(role)
+                        RoleItem(roles: $roles, role: role)
+                    }
+                }
+                .onAppear() {
+                    self.roleViewModel.fetchData()
+                }
+                .padding()
+            }
+            
         }
-        .onAppear() {
-            self.viewModel.fetchData()
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "rectangle.portrait.and.arrow.right").rotationEffect(.degrees(180))
+                })
+                .foregroundColor(.black)
+            }
         }
     }
 }
 
 struct RoleItem: View {
-    @State var roleImage: Image
-    @State var roleName: String
+    @Binding var roles: [Role]
+    @State var isModalShown = false
+    @State var role: Role
     
     var body: some View {
-        ZStack {
-            VStack {
-                roleImage.resizable().frame(width: 64, height: 64)
-                Text("\(roleImage)")
+        Button(action: {
+            roles.append(role)
+            isModalShown = true
+        }) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.black)
+                VStack {
+                    Image("\(role.roleName)")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: imageWidthSize, height: imageHeightSize)
+                    Text("\(role.roleName)").padding(.bottom).foregroundColor(.black)
+                }
+                if role.id < 3 {
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.circle.fill")
+                                .foregroundColor(Color.black)
+                                .padding(10)
+                            Spacer()
+                        }
+                        Spacer()
+                    }
+                }
             }
-            RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(Color.white)
-            RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(Color.red).padding(EdgeInsets(top: 15, leading: 15, bottom: 0, trailing: 0))
+        }.sheet(isPresented: $isModalShown) {
+            NavigationView {
+                EmptyView(role: role)
+            }
         }
+        .padding(.leading)
+        .padding(.trailing)
     }
 }
 
 //struct RoleSelectView_Previews: PreviewProvider {
 //    static var previews: some View {
-//        RoleSelectView()
+//        RoleSelectView(roomCode: "1Q2W3E")
 //    }
 //}
