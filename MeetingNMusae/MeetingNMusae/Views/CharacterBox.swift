e0111ce5a6422159c5394df298e3eb1065fd6c2b
Taeho Kim <xogh8025@gmail.com>
2022-06-12 21:03:55 +0900

    #7 feat: add color list for color shadow

--- MeetingNMusae/MeetingNMusae/Views/CharacterBox.swift
+++ MeetingNMusae/MeetingNMusae/Views/CharacterBox.swift
@@ -7,17 +7,24 @@
 import SwiftUI
 
 struct CharacterBox: View {
+    let colorList: [Color] = [.black, .musaeBlue, .musaeOrange, .musaeMint,
+                              .musaeGreen, .musaeLightGreen, .musaePurple,
+                              .musaeSkyBlue, .musaePink, .musaeRed, .musaeMustard]
     let width: CGFloat?
     let height: CGFloat?
-    init(width: CGFloat? = nil, height: CGFloat? = nil) {
+    var roleIndex: Int?
+    
+    init(width: CGFloat? = nil, height: CGFloat? = nil, roleIndex:Int = 0) {
         self.width = width
         self.height = height
+        self.roleIndex = roleIndex
     }
+    
     var body: some View {
         RoundedRectangle(cornerRadius: 12)
             .foregroundColor(.white)
             .frame(width: width, height: height)
-            .shadow(color: .black, radius: 0, x: 8, y: 8)
+            .shadow(color: colorList[roleIndex ?? 0], radius: 0, x: 8, y: 8)
             .overlay(RoundedRectangle(cornerRadius: 12)
                     .stroke(Color.black, lineWidth: 3)
             )
