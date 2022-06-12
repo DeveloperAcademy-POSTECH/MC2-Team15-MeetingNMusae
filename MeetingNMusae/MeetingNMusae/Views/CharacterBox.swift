748a9f5b0e4f1985904532f1d25fc794559b6d06
Taeho Kim <xogh8025@gmail.com>
2022-06-12 19:46:00 +0900

    #7 refactor: add initializer with nil

--- MeetingNMusae/MeetingNMusae/Views/CharacterBox.swift
+++ MeetingNMusae/MeetingNMusae/Views/CharacterBox.swift
@@ -4,20 +4,22 @@
 //
 //  Created by 김태호 on 2022/06/11.
 //
-
-import Foundation
 import SwiftUI
 
 struct CharacterBox: View {
     let width: CGFloat?
-    let height: CGFloat
+    let height: CGFloat?
+    init(width: CGFloat? = nil, height: CGFloat? = nil) {
+        self.width = width
+        self.height = height
+    }
     
     var body: some View {
         RoundedRectangle(cornerRadius: 12)
             .foregroundColor(.white)
             .frame(width: width, height: height)
             .shadow(color: .black, radius: 0,  x: 8, y: 8)
-            .overlay(
+            .overlay (
                 RoundedRectangle(cornerRadius: 12)
                     .stroke(Color.black, lineWidth: 3)
             )
