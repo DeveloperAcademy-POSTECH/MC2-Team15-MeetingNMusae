559a2dec4b7e069526019c13a3515d94b8c72dd3
HeejiSohn <96990839+HeejiSohn@users.noreply.github.com>
2022-06-16 11:23:59 +0900

    add image and text ui

--- MeetingNMusae/MeetingNMusae/Views/BestPlayerShowingView.swift
+++ MeetingNMusae/MeetingNMusae/Views/BestPlayerShowingView.swift
@@ -8,15 +8,41 @@
 import SwiftUI
 
 struct BestPlayerShowingView: View {
+    
+    let imageName = "금고무새"
     var body: some View {
         
         ZStack {
+
+            VStack {
+                Text("최고의 플레이어!")
+                    .font(.system(size:24))
+                    .fontWeight(.bold)
+                    .padding(.bottom, 92)
+                    .padding(.top, 30)
+                
+                VStack {
+    
+                    Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:155,height:155)
+                }
+                .background(CharacterBox())
+                Spacer()
+            }
+            
             LottieView(name: "confetti", loopMode: .loop)
-                        .scaledToFill()
-                    .ignoresSafeArea(edges:.top)
+                .scaledToFill()
+                .ignoresSafeArea(edges:.top)
+            
         }
+        
     }
+    
 }
+
+
 
 struct BestPlayerShowingView_Previews: PreviewProvider {
     static var previews: some View {
