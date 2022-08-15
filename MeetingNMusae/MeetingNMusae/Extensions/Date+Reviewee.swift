268ffb832b614eca7d8d32eb2eec4774551a5aad
Taekyun Kim <taekkim@student.42seoul.kr>
2022-08-15 12:36:55 +0900

    feat: 랜덤 함수 로직 변경

--- MeetingNMusae/MeetingNMusae/Extensions/Date+Reviewee.swift
+++ MeetingNMusae/MeetingNMusae/Extensions/Date+Reviewee.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 extension Date {
+    // index: 유저인덱스
+    // num: 총 사람 명수
     static func getRevieweeIndex(index: Int, num: Int) -> Int {
-        // 자정에 하면 클남
-        
-        var revieweeIndex = (index + Int(Date.now.timeIntervalSince1970)) / (60 * 60 * 24) % num
-        
-        while revieweeIndex == index {
-            revieweeIndex = (index + Int(Date.now.timeIntervalSince1970)) / (60 * 60 * 24) % num
+        var revieweeIndex = (index + ((Int(Date.now.timeIntervalSince1970) / (60 * 60 * 24)) % num)) % num
+
+        if revieweeIndex == index {
+            revieweeIndex = (revieweeIndex + 1) % num
         }
         return revieweeIndex
     }
