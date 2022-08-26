c4099b169c7169c17d00dff2c03a9f5d7e03e824
EuniceNam <caellime@gmail.com>
2022-08-26 16:41:55 +0900

    refactor: 디버깅 라인 표시 방법 수정
    코드리뷰 반영

--- MeetingNMusae/MeetingNMusae/Views/MissionView.swift
+++ MeetingNMusae/MeetingNMusae/Views/MissionView.swift
@@ -6,18 +6,19 @@ struct MissionView: View {
     @ObservedObject var userViewModel = UserViewModel()
     // 진행자가 회의 종료를 선택하면 참가자도 자동으로 다음 뷰로 넘어가기 위해 필요
     @ObservedObject var meetingRoomViewModel = MeetingRoomViewModel()
-    
-    // 임시
+    // 이 뷰에서 미션 내용을 가져오고, 미션카드뷰에서 미션 내용을 읽어오기 위해 필요.
+    // 없이 하면 미션카드뷰에서 미션 내용이 로딩 안 됨.
     @ObservedObject var missionViewModel = MissionViewModel()
 
-    // debug
-//    let roomCode = "D4M3KK"
-//    let nickname = "EUNI"
-//    let roleId = 1
-
+    #if DEBUG
+    let roomCode = "D4M3KK"
+    let nickname = "EUNI"
+    let roleId = 1
+    #else
     let roomCode = UserDefaults.standard.string(forKey: "roomCode") ?? ""
     let nickname = UserDefaults.standard.string(forKey: "nickname") ?? ""
     let roleId = UserDefaults.standard.integer(forKey: "roleId")
+    #endif
 
     // 폰트 크기에 따라 수정 예정
     let textheight: CGFloat = 34 // UIScreen.screenHeight * 0.06
@@ -29,7 +30,6 @@ struct MissionView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
                     // MARK: 역할 순서에 맞게 변경 필요
-                    // users에서 정렬..?
                     ForEach(userViewModel.users) { user in
                         // 넘길 데이터: 참여자별 역할, 닉네임, 미션 달성도
                         // MARK: 넘길 데이터: 화면비에 해당하는 프로그레스 바 두께
@@ -59,7 +59,6 @@ struct MissionView: View {
             if roleId == 1 {
                 Button {
                     // 파이어베이스에 회의종료 set하기
-//                    meetingRoomViewModel.updateIsEnded(roomCode: roomCode)
                     meetingRoomViewModel.endMeeting(roomCode: roomCode)
                 } label: {
                     Text("회의 종료하기")
@@ -67,13 +66,12 @@ struct MissionView: View {
                         .font(.headline)
                         .fontWeight(.heavy)
                         .foregroundColor(.buttonGray)
-//                        .padding()
 //                        .padding(54)
                 }
             } else {
                 EmptyView()
             }
-            Spacer()// .frame(height: 2)
+            Spacer()
         }
         .onAppear {
             self.userViewModel.fetchData(roomCode: roomCode, nickname: nickname, sort: true)
